afd1839824566a7e22f0fc1439c06835751dd5e9
ZedAshkan <mahdimomenihv@gmail.com>
2021-10-21 09:58:43 +0330

    add weather icons

--- vanillajs-weatherApp/script.js
+++ vanillajs-weatherApp/script.js
@@ -1,25 +1,46 @@
-const key = 'afiuA1gzAsFvSe1qFGbjAyjO6TUfKqIO'
+const key = 'XizEmrDdtWDnkgXr9hLEbEjv54mnMubV'
 const form = document.querySelector('#form')
 const city = document.querySelector('#city')
 const temp = document.querySelector('#temp')
+const box = document.querySelector('#box')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
 
     cityV = form.querySelector('input').value.trim();
 
-    city.innerHTML = cityV
-
-    getCity(cityV)
-        .then(data => {
-            return getWeather(data.Key)
-        }).then(data => {
-            return data.Temperature.Metric.Value
-        }).then(data => temp.innerHTML = data)
-        .catch(err => console.log(err))
-
+    updateCity(cityV)
     form.reset()
 })
+
+const updateCity = async(cityV)=>{
+
+    const cityCode = await getCity(cityV)
+    const weather = await getWeather(cityCode.Key)
+
+    const temperature = weather.Temperature.Metric.Value
+    const country = cityCode.Country.ID
+    const province = cityCode.AdministrativeArea.EnglishName
+    const WeatherIcon = weather.WeatherIcon
+    const WeatherText = weather.WeatherText
+    const cityEnglishName = cityCode.EnglishName
+
+    box.innerHTML = `
+    <div class="card w-75 mx-auto m-3 " style="width: 18rem;">
+			<img src="https://picsum.photos/400/300" style="height: 300px;" class="card-img-top" alt="...">
+            <div class="card-body position-relative">
+				<img src="./assets/icons/${WeatherIcon}.svg" class="w-icons">
+                <h5 class="card-title text-center fs-1 mt-5">
+						<span id="temp">${temperature}</span>
+						<span>&deg;C</span>
+				</h5>
+                <p class="card-text text-center fs-6">${WeatherText}</p>
+				<p id="city" class="card-text text-center fs-1 fw-bold mb-0">${cityEnglishName}</p>
+                <p class="card-text text-center fs-6">${country} , ${province}</p>
+			</div>
+		</div>
+    `
+}
 
 const getWeather = async (id) => {
 
